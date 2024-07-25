002202946683ba4f6d3f469f3c923d3b62ffb1e4
MaxenV <maxen2001@gmail.com>
2024-07-25 18:57:55 +0200

    Create get_audio paths
    function return dictionary of class_names and paths to audio files
    
    it is used in __init__

--- code/helpers/audio_process.py
+++ code/helpers/audio_process.py
@@ -13,5 +13,17 @@ class AudioProcess:
         )
         self.sampling_rate = sampling_rate or int(os.getenv("SAMPLING_RATE"))
 
-        self.paths = {}
+        self.paths = self.get_audio_paths(self.audio_folder)
         self.melspectrograms = {"class_names": [], "data": []}
+
+    def get_audio_paths(self, audio_folder):
+        paths = {}
+        for root, _, files in os.walk(audio_folder):
+            if root != audio_folder:
+                name = os.path.basename(root)
+                for file in files:
+                    if name in paths.keys():
+                        paths[name].append(os.path.join(root, file))
+                    else:
+                        paths[name] = [os.path.join(root, file)]
+        return paths
